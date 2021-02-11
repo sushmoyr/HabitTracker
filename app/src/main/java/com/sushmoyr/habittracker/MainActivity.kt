41c0722dae5662160c632678aece547d17d5a3fb
Sushmoy Roy <34061341+Ex-Mach1na@users.noreply.github.com>
2021-02-11 14:31:58 +0600

    Initial commit

---
+++ app/src/main/java/com/sushmoyr/habittracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sushmoyr.habittracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
